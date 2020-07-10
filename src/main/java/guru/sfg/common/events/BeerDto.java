40d11e4a9b2c016117e7ecb2f6c206195e4607bc
ericlee83 <eric-lee-83@hotmail.com>
2020-07-10 20:55:22 +1000

    init

---
+++ src/main/java/guru/sfg/common/events/BeerDto.java
@@ -0,0 +1,57 @@
+package guru.sfg.common.events;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateSerializer;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.*;
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.time.OffsetDateTime;
+import java.util.UUID;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class BeerDto implements Serializable {
+
+    private static final long serialVersionUID = 7697714161324920433L;
+
+    @JsonProperty("beerId")
+    @Null
+    private UUID id;
+    @Null
+    private Integer version;
+    @Null
+    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ssZ", shape = JsonFormat.Shape.STRING)
+    private OffsetDateTime createdDate;
+    @Null
+    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ssZ", shape = JsonFormat.Shape.STRING)
+    private OffsetDateTime lastModifiedDate;
+    @NotBlank
+    @Size(min = 3, max = 100)
+    private String beerName;
+    @NotNull
+    private String beerStyle;
+
+    @NotNull
+    private String upc;
+    @Positive
+    @NotNull
+    @JsonFormat(shape = JsonFormat.Shape.STRING)
+    private BigDecimal price;
+    @PositiveOrZero
+    private Integer quantityOnHand;
+    @JsonSerialize(using = LocalDateSerializer.class)
+    @JsonDeserialize(using = LocalDateDeserializer.class)
+    private LocalDate mylocalDate;
+}
